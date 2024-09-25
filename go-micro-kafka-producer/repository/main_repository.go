cd51b3bfae7658490922dd6f96925b58f34cf178
fajaramaulana <fajaragusmaulana@gmail.com>
2024-09-25 11:31:19 +0700

    Refactor main repository implementation and interface

--- go-micro-kafka-producer/repository/main_repository.go
+++ go-micro-kafka-producer/repository/main_repository.go
@@ -1,39 +1,7 @@
 package repository
 
-import (
-	"strconv"
+import "github.com/fajaramaulana/go-micro-kafka/go-micro-kafka-producer/model/response"
 
-	"github.com/fajaramaulana/go-micro-kafka/go-micro-kafka-producer/model/response"
-	"github.com/google/uuid"
-)
-
-func NewMainRepository() MainRepository {
-	return &mainRepositoryImpl{}
-}
-
-type mainRepositoryImpl struct {
-}
-
-func (m *mainRepositoryImpl) GetData() (*response.GetData, error) {
-	// data detail
-
-	count := 5
-	dataDetail := []response.DataDetail{}
-	for i := 0; i < count; i++ {
-		dataDetail = append(dataDetail, response.DataDetail{
-			Uuid: uuid.New().String(),
-			Name: "Name " + strconv.Itoa(i),
-			Age:  20,
-		})
-	}
-
-	retData := response.GetData{
-		Message: "Hello World",
-		Status:  true,
-		Time:    "2024-09-24",
-		Data:    dataDetail,
-	}
-
-	return &retData, nil
-
+type MainRepository interface {
+	GetData() (*response.GetData, error)
 }
